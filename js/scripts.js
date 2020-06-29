78aa047fecf5b86369cd7e18d016b5b33ce65f12
paramthakker <37357102+paramthakker@users.noreply.github.com>
2020-06-29 12:30:03 +0530

    Minor Updates

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,9 @@
     * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
     */
     (function ($) {
-    "use strict"; // Start of use strict
+    "use strict";
 
-    // Smooth scrolling using jQuery easing
+
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (
             location.pathname.replace(/^\//, "") ==
@@ -30,13 +30,12 @@
         }
     });
 
-    // Closes responsive menu when a scroll trigger link is clicked
     $(".js-scroll-trigger").click(function () {
         $(".navbar-collapse").collapse("hide");
     });
 
-    // Activate scrollspy to add active class to navbar items on scroll
+    
     $("body").scrollspy({
         target: "#sideNav",
     });
-})(jQuery); // End of use strict
+})(jQuery); 
